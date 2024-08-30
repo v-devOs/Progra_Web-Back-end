5ace85396429e88aa09dd02eaca9a265119f7eaa
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-08-29 20:22:37 -0600

    Initial commit

---
+++ src/main/java/com/example/Spring/Back_end/SpringBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring/Back_end/SpringBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
